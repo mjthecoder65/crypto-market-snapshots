35ccc1e26ac1977f36fadfd12aa83e68028af3d0
Michael <mjthecoder53161@gmail.com>
2024-08-18 17:48:27 +0900

    update: renamed FetchCandleEveryTwoMinutes to StartCandleJobs

--- fetchers/candles.go
+++ fetchers/candles.go
@@ -2,19 +2,25 @@ package fetchers
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"log"
 	"strconv"
 	"time"
 
 	"github.com/mjthecoder65/crypto-market-snapshots/models"
 )
 
-func FetchCandles(symbol string, interval string, limit int, candleChannel chan<- models.Candle) {
+func FetchCandles(symbol string, interval string, limit int) ([]models.Candle, error) {
 	klineService := client.NewKlinesService().Symbol(symbol).Interval(interval).Limit(limit)
 	klines, err := klineService.Do(context.Background())
 
 	if err != nil {
-		return
+		errorMessage := fmt.Sprintf("error: %v", err)
+		return nil, errors.New(errorMessage)
 	}
+
+	candles := []models.Candle{}
 
 	for _, kline := range klines {
 		open, _ := strconv.ParseFloat(kline.Open, 64)
@@ -34,14 +40,31 @@ func FetchCandles(symbol string, interval string, limit int, candleChannel chan<
 			Low:       low,
 			Volume:    volume,
 		}
-		candleChannel <- candle
+		candles = append(candles, candle)
 	}
+
+	return candles, nil
 }
 
-func FetchCandleEveryTwoMinutes(symbol string, interval string, limit int, candleChannel chan models.Candle) {
-	ticker := time.NewTicker(time.Minute / 5)
+func SendCandleMessage(candle models.Candle, sender chan<- models.Candle) {
+	sender <- candle
+	log.Printf("Sent candle: %v", candle)
+}
+
+func StartCandleJob(symbol string, interval string, limit int, candleChannel chan models.Candle) {
+	ticker := time.NewTicker(1 * time.Minute)
 	defer ticker.Stop()
+
 	for range ticker.C {
-		FetchCandles(symbol, interval, limit, candleChannel)
+		candles, err := FetchCandles(symbol, interval, limit)
+
+		if err != nil {
+			log.Printf("failed to fetch candle with params(symbol: %s interval: %s limit: %d)", symbol, interval, limit)
+			continue
+		}
+
+		for _, candle := range candles {
+			SendCandleMessage(candle, candleChannel)
+		}
 	}
 }
